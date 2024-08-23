0ced0e17a3deadc8313e0a3afb46e5b6a1dc2776
Vedank Naik <vedankn01@gmail.com>
2024-08-23 13:03:09 +0530

    Homepage, forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+import { PatientForm } from "@/components/ui/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <p>hello</p>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex items-center align-middle mb-12">
+            <Image
+              src="/assets/icons/logo.png"
+              height={1000}
+              width={1000}
+              alt="logo"
+              className="h-12 w-fit"
+            />
+            <p className="ml-2">HealthHub</p>
+          </div>
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © {new Date().getFullYear()} HealthHub
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="onboarding"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
