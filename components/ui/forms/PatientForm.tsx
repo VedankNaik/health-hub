83443ab7768eacf51b22090f7c1e1062e2978412
Vedank Naik <vedankn01@gmail.com>
2024-10-21 19:45:43 +0530

    Connect with appwrite

--- components/ui/forms/PatientForm.tsx
+++ components/ui/forms/PatientForm.tsx
@@ -29,23 +29,19 @@ export const PatientForm = () => {
 
   const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
     setIsLoading(true);
-
     try {
       const user = {
         name: values.name,
         email: values.email,
         phone: values.phone,
       };
-
       const newUser = await createUser(user);
-
       if (newUser) {
         router.push(`/patients/${newUser.$id}/register`);
       }
     } catch (error) {
       console.log(error);
     }
-
     setIsLoading(false);
   };
 
@@ -56,7 +52,6 @@ export const PatientForm = () => {
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Get started with appointments.</p>
         </section>
-
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
@@ -66,7 +61,6 @@ export const PatientForm = () => {
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
-
         <CustomFormField
           fieldType={FormFieldType.INPUT}
           control={form.control}
@@ -76,7 +70,6 @@ export const PatientForm = () => {
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
-
         <CustomFormField
           fieldType={FormFieldType.PHONE_INPUT}
           control={form.control}
@@ -84,7 +77,6 @@ export const PatientForm = () => {
           label="Phone number"
           placeholder="(555) 123-4567"
         />
-
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
